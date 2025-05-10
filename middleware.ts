66223ea5694517f5c9527e3f6ead460b32dff587
Taral92 <taru.patel37225@gmail.com>
2025-05-10 23:52:17 +0530

    cleark login added

--- middleware.ts
+++ middleware.ts
@@ -2,8 +2,8 @@ import {clerkMiddleware,createRouteMatcher} from "@clerk/nextjs/server";
 import {NextResponse} from "next/server"
 
 const ispublicroute =createRouteMatcher([
-    '/signin',
-    '/signup',
+    '/sign-in',
+    '/sign-up',
     '/home',
     
 ])
@@ -22,11 +22,11 @@ export default clerkMiddleware(async(auth,req)=>{
       // not logged in
       if(!userId){
         if(!ispublicapiroute(req) && !ispublicroute(req)){
-            return NextResponse.redirect(new URL('/signin',req.url))
+            return NextResponse.redirect(new URL('/sign-in',req.url))
         }
       }
       if(isapirequest && !ispublicapiroute(req)){
-        return NextResponse.redirect(new URL('/signin',req.url))
+        return NextResponse.redirect(new URL('/sign-in',req.url))
       }
 });
 export const config = {
@@ -35,5 +35,6 @@ export const config = {
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     // Always run for API routes
     '/(api|trpc)(.*)',
+    '/socialshare(.*)',
   ],
 };
