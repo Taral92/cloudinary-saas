3f1b68e742376e9590caddc304e64b896cdfb2ed
Taral92 <taru.patel37225@gmail.com>
2025-05-15 00:45:56 +0530

    fix

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    extends: [
+      'next/core-web-vitals',
+      'plugin:@typescript-eslint/recommended',
+    ],
+    rules: {
+      '@typescript-eslint/no-this-alias': 'off',
+      '@typescript-eslint/no-unused-expressions': 'off',
+    },
+  };
